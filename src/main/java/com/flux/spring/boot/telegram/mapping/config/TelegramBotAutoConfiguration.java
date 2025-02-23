c6604d664888aa325c87f7470d02944891bff2af
taragrammatonn <qweqwe@qwe.qwe>
2025-02-23 12:58:47 +0200

    Exclude redundant EnableTelegramBot annotation

--- src/main/java/com/flux/spring/boot/telegram/mapping/config/TelegramBotAutoConfiguration.java
+++ src/main/java/com/flux/spring/boot/telegram/mapping/config/TelegramBotAutoConfiguration.java
@@ -1,16 +1,31 @@
 package com.flux.spring.boot.telegram.mapping.config;
 
 import com.flux.spring.boot.telegram.mapping.core.BotRequestProcessor;
-import com.flux.spring.boot.telegram.mapping.core.ReactiveUpdateConsumer;
+import lombok.AllArgsConstructor;
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.starter.TelegramBotInitializer;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
+@AllArgsConstructor
 @EnableConfigurationProperties(TelegramBotProperties.class)
 public class TelegramBotAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public BotRequestProcessor botRequestProcessor() {
+        return new BotRequestProcessor();
+    }
 
     @Bean
     @ConditionalOnMissingBean
@@ -19,14 +34,32 @@ public class TelegramBotAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean
-    public ReactiveUpdateConsumer reactiveUpdateConsumer(TelegramBotProperties properties, BotRequestProcessor botRequestProcessor) {
-        return new ReactiveUpdateConsumer(properties.getToken(), botRequestProcessor);
+    public SpringLongPollingBot springLongPollingBot(TelegramBotProperties properties,
+                                                     BotRequestProcessor botRequestProcessor) {
+        return new SpringLongPollingBot() {
+            @Override
+            public String getBotToken() {
+                return properties.getToken();
+            }
+
+            @Override
+            public LongPollingUpdateConsumer getUpdatesConsumer() {
+                return updates -> updates.forEach(botRequestProcessor::handleUpdate);
+            }
+        };
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(TelegramBotsLongPollingApplication.class)
+    public TelegramBotsLongPollingApplication telegramBotsApplication() {
+        return new TelegramBotsLongPollingApplication();
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public BotRequestProcessor botRequestProcessor() {
-        return new BotRequestProcessor();
+    public TelegramBotInitializer telegramBotInitializer(TelegramBotsLongPollingApplication telegramBotsApplication,
+                                                         ObjectProvider<List<SpringLongPollingBot>> longPollingBots) {
+        return new TelegramBotInitializer(telegramBotsApplication,
+                longPollingBots.getIfAvailable(Collections::emptyList));
     }
 }
